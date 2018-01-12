bc19fe8d48f79d3e69eab0b79765125efcb33199
EthanZ <ezhang@phoceis.asia>
2018-01-11 22:00:14 -0800

    init project

---
+++ src/renderer/components/homePage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="wrapper">
+        This is the homepage.
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'home-page'
+  }
+</script>
+
+<style>
+    #wrapper {
+        font-size: 10rem;
+    }
+</style>
