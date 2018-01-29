e23820fc1e7f469a3f86b2164e2b40c85e506eeb
EthanZ <ezhang@phoceis.asia>
2018-01-28 18:07:16 -0800

    Move to VScode

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -11,8 +11,10 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  components: { App },
-  router,
-  store,
-  template: '<App/>'
-}).$mount('#app')
+		components: { App },
+		router,
+		store,
+		template: '<App/>'
+	})
+.$mount('#app')
+	
